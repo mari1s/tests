510ff68a47feaccdd6f38fe2f578b7d273462031
mari1s <112856912+mari1s@users.noreply.github.com>
2024-04-22 15:36:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ function sutitZinu()
 {
     console.log('sutitZinu()darbojas');
     zinas.innerHTML = zinas.innerHTML+'<br>'+ zina.value ;
-
+}
+async function ieladetChataZinas()
+{
+    let datiNoServera= await fetch('chatazinas.txt');
+    let dati=await datiNoServera.text;
+    
 }
